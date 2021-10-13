115d8cbb1b2f3f1c9af1015b60b6a443002aeab4
Edison-Du <83848572+Edison-Du@users.noreply.github.com>
2021-10-13 11:04:25 -0400

    Add files via upload

--- FloorPlanSystem.java
+++ FloorPlanSystem.java
@@ -1,64 +1,378 @@
+/******* Graphics and GUI imports *******/
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Rectangle;
+
+/******* Keyboard imports *******/
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/******* Mouse imports *******/
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+/******* Utility imports *******/
 import java.util.ArrayList;
+import java.util.Random;
 
+/**
+ * This class manages a list of Table objects. It creates an
+ * interactive JFrame that allows the user the manage the
+ * floor plan of the tables.
+ * 
+ * @author Edison Du
+ * @author Peter Gu
+ * @author Jeffrey Xu
+ */
 public class FloorPlanSystem {
-    private ArrayList<ArrayList<Table>> tableList;
-    private final String[] GROUP_NAMES = { "intro", "contest", "web" };
-    private final int NUM_GROUPS = 3;
 
-    // private final int e
+    /***** Window Dimensions *****/
+    private final int FRAME_WIDTH = 1300;
+    private final int FRAME_HEIGHT = 760;
 
-    public FloorPlanSystem() {
-        // tableList = new ArrayList<ArrayList<Table>>(NUM_GROUPS);
-        tableList = new ArrayList<ArrayList<Table>>();
-        for (int i = 0; i < NUM_GROUPS; i++) {
-            tableList.add(new ArrayList<Table>());
+    /***** Graphics Related ******/
+    private final int GRID_SQUARE_SIZE = 30;
+    private final int MAX_RGB = 255;
+    private final int LEGEND_X_POSITION = 1120;
+    private final int LEGEND_Y_POSITION = 30;
+    private final int MOUSE_X_OFFSET = -10;
+    private final int MOUSE_Y_OFFSET = -30;
+    private final int EXIT_BUTTON_X = GRID_SQUARE_SIZE;
+    private final int EXIT_BUTTON_Y = GRID_SQUARE_SIZE * 21;
+    private final int EXIT_BUTTON_WIDTH = GRID_SQUARE_SIZE*5;
+    private final int EXIT_BUTTON_HEIGHT = GRID_SQUARE_SIZE*2;
+    private final int TABLE_HELPER_X = FRAME_WIDTH-440;
+    private final int TABLE_HELPER_Y = FRAME_HEIGHT-220;
+    private final Color BUTTON_COLOR = new Color(125, 125, 255);
+
+    /***** Camera Shift *****/
+    private int offsetX = 0;
+    private int offsetY = 0;
+
+    /***** Program Logic ******/
+    private int numTables;
+    private int numGroups;
+    private String[] groups;
+    private Color[] groupColors;
+    private ArrayList<Table> tableList;
+
+    /***** Table Movement *****/
+    private int selectedTable;
+    private int selectedTableX, selectedTableY;
+    private boolean selected;
+    
+    /***** Utility ******/
+    private Random random = new Random();
+    
+    /**
+     * Constructs a new floor plan system with a list of Table objects.
+     * @param groups The different groups that the tables belong to
+     */
+    public FloorPlanSystem(String[] groups) {
+        this.groups = groups;
+        this.numGroups = groups.length;
+        tableList = new ArrayList<Table>();
+        groupColors = new Color[numGroups];
+        selectedTable = -1;
+        
+        // Assign colors associated with each group
+        for (int i = 0; i < numGroups; i++) {
+            int r = random.nextInt(MAX_RGB/2) + MAX_RGB/2;
+            int g = random.nextInt(MAX_RGB/2) + MAX_RGB/2;
+            int b = random.nextInt(MAX_RGB/2) + MAX_RGB/2;
+            groupColors[i] = new Color(r, g, b);
         }
     }
 
-    public void displayTables(Graphics g) {
-
-        int xPosition = 50;
-
-        for (int i = 0; i < NUM_GROUPS; i++) {
-            int curCol = 0, curRow = 0;
-
-            for (Table t : tableList.get(i)) {
-                t.draw(g, xPosition + curCol * 200, curRow * 200);
-                curCol = (curCol + 1) % 2;
-                // curCol ^= 0;
-                if (curCol == 0)
-                    curRow++;
-            }
-
-            g.drawString(GROUP_NAMES[i], xPosition + 50, 10);
-            xPosition += 600;
-        }
+    /**
+     * Creates an interactive JFrame used to display all the
+     * tables and allows the user to move them around.
+     */
+    public void displayTables() {
+        FloorPlanFrame floorPlanFrame = new FloorPlanFrame();
     }
 
+    /**
+     * Adds a table object to the floor plan's list of tables.
+     * @param table the table object to add
+     * @param groupName the group associated with the table
+     * @return whether the table was succesfully added
+     */
     public boolean addTable(Table table, String groupName) {
-        for (int i = 0; i < NUM_GROUPS; i++) {
-            if (GROUP_NAMES[i].equals(groupName)) {
-                tableList.get(i).add(table);
+        for (int i = 0; i < numGroups; i++) {
+            if (groups[i].equals(groupName)) {
+
+                table.setColor(groupColors[i]);
+                table.setGroup(groupName);
+                tableList.add(table);
+                numTables++;
+
+                // Reset the table layout to accommodate for the new table
+                rearrangeTables();
+                return true;
+            }
+        }
+        // If none of the groups match the group name, the table is not added
+        return false;
+    }
+    
+    /**
+     * Removes a table object from the floor plan's list of tables.
+     * @param id the id of the table to remove
+     * @return whether the table exists and was succesfully removed
+     */
+    public boolean removeTable(int id) {
+        for (int i = 0; i < numTables; i++) {
+            if (tableList.get(i).getID() == id) {
+                tableList.remove(i);
+                numTables--;  
                 return true;
             }
         }
         return false;
     }
 
-    public boolean removeTable(int id) {
-        for (int i = 0; i < NUM_GROUPS; i++) {
-            for (int j = tableList.get(i).size() - 1; j >= 0; j--) {
-                if (tableList.get(i).get(j).getID() == id) {
-                    tableList.get(i).remove(j);
-                    return true;
-                }
-            }
-        }
-        return false;
+    /**
+     * Returns an arraylist containing the list of tables.
+     * @return the arraylist containing the list of tables
+     */
+    public ArrayList<Table> getTableList() {
+        return this.tableList;
     }
 
-    public ArrayList<ArrayList<Table>> getTableList() {
-        return this.tableList;
+    /**
+     * Rearranges all the tables into a grid formation in the floor 
+     * plan layout.
+     */
+    private void rearrangeTables() {
+        int currentRow = GRID_SQUARE_SIZE;
+        int currentColumn = GRID_SQUARE_SIZE;
+
+        for (int i = 0; i < numTables; i++) {
+            
+            tableList.get(i).setX(currentColumn);
+            tableList.get(i).setY(currentRow);
+            
+            /**
+             *  Make sure that the table moves onto the next row of the grid
+             *  when it reaches the end of the JFrame window.
+             */
+            currentColumn += Table.WIDTH + GRID_SQUARE_SIZE;
+            if (currentColumn + GRID_SQUARE_SIZE >= FRAME_WIDTH) {
+                currentColumn = GRID_SQUARE_SIZE;
+                currentRow += Table.HEIGHT + GRID_SQUARE_SIZE;
+            }
+        }
+    }
+
+    private class FloorPlanFrame extends JFrame {
+
+        FloorPlanFrame() {
+            super("Floor Plan System");
+
+            // Basic initialization
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+            this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+            this.setResizable(false);
+            this.requestFocusInWindow();
+            this.setVisible(true);   
+
+            // Add panel
+            this.add(new FloorPlanPanel());
+
+            // Add listeners
+            this.addKeyListener(new KeyEventListener());    
+            this.addMouseListener(new MouseEventListener());  
+            this.addMouseMotionListener(new MouseMotionEventListener());
+        
+            // Create thread
+            Thread t = new Thread(new Runnable(){     
+                public void run() { 
+                    animate();
+                }
+            }); 
+              
+            t.start();  
+        }
+        
+        private void animate() {
+            while (true) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread Interrupted");
+                    e.printStackTrace();
+                }
+                this.repaint();
+            }
+        }
+        
+        // Inner class for JPanel
+        private class FloorPlanPanel extends JPanel {
+
+            public void paintComponent(Graphics g) {
+                setDoubleBuffered(true);
+
+                // Draw grid lines
+                g.setColor(new Color(200, 200, 200));
+                for (int i = 0; i < FRAME_WIDTH; i += GRID_SQUARE_SIZE) {
+                    g.drawLine(i, 0, i, FRAME_HEIGHT);
+                }
+                for (int i = 0; i < FRAME_HEIGHT; i += GRID_SQUARE_SIZE) {
+                    g.drawLine(0, i, FRAME_WIDTH, i);
+                }
+
+                // Draw tables
+                for (int i = 0; i < numTables; i++) {
+                    if (i != selectedTable) {
+                        tableList.get(i).draw(g, offsetX, offsetY);
+                    }
+                }
+
+                // Draw table hover helper
+                if (selected) {
+                    tableList.get(selectedTable).drawTransparent(g, offsetX, offsetY);    
+                    tableList.get(selectedTable).drawInformation(g, TABLE_HELPER_X, TABLE_HELPER_Y);
+                }
+
+                // Draw color legend for groups
+                g.setColor(new Color(255, 255, 255, 130));
+                g.fillRect(LEGEND_X_POSITION, LEGEND_Y_POSITION, GRID_SQUARE_SIZE * 4, GRID_SQUARE_SIZE * 4);
+                g.setColor(Color.black);
+                g.drawRect(LEGEND_X_POSITION, LEGEND_Y_POSITION, GRID_SQUARE_SIZE * 4, GRID_SQUARE_SIZE * 4);
+                
+                for (int i = 0; i < numGroups; i++) {
+                    int labelX = LEGEND_X_POSITION+15;
+                    int labelY = i * GRID_SQUARE_SIZE + LEGEND_Y_POSITION+15;
+                    g.setColor(groupColors[i]);
+                    g.fillRect(labelX, labelY, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE);
+                    g.setColor(Color.black);
+                    g.drawRect(labelX, labelY, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE);
+                    g.drawString(groups[i], labelX + GRID_SQUARE_SIZE + 15, labelY + 20);
+                }
+
+                // Draw button
+                g.setColor(BUTTON_COLOR);
+                g.fillRect(EXIT_BUTTON_X, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+                g.setColor(Color.black);
+                g.drawRect(EXIT_BUTTON_X, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+                g.drawString("EXIT", EXIT_BUTTON_X + 25, EXIT_BUTTON_Y + 25);
+
+            }
+        }
+
+        // Key listener
+        private class KeyEventListener implements KeyListener {
+            public void keyPressed (KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    FloorPlanFrame.this.dispose();
+                } else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+                    offsetX -= GRID_SQUARE_SIZE;
+                } else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+                    offsetX += GRID_SQUARE_SIZE;
+                } else if (e.getKeyCode() == KeyEvent.VK_UP){
+                    offsetY += GRID_SQUARE_SIZE;
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+                    offsetY -= GRID_SQUARE_SIZE;
+                }
+            }
+            public void keyReleased (KeyEvent e) {}
+            public void keyTyped (KeyEvent e) {}
+        }
+
+        // Mouse listener
+        private class MouseEventListener implements MouseListener {
+            public void mousePressed (MouseEvent e) {
+                
+                // Instatiate mouse coordinates 
+                int mouseX = e.getX() + MOUSE_X_OFFSET;
+                int mouseY = e.getY() + MOUSE_Y_OFFSET;
+
+                // Dispose the frame if exit button is pressed
+                if ( (mouseX >= EXIT_BUTTON_X) && (mouseX <= EXIT_BUTTON_X + EXIT_BUTTON_WIDTH) ) {
+                    if ( (mouseY >= EXIT_BUTTON_Y) && (mouseY <= EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT) ) {
+                        FloorPlanFrame.this.dispose();
+                    }
+                }
+
+                // Move table
+                if (!selected){
+                    mouseX -= offsetX;
+                    mouseY -= offsetY;
+                    // Checking for which table is currently being cclicked
+                    for (int i = 0; i < numTables; i++) {
+                        Table t = tableList.get(i);
+                        if (t.checkOverlap(mouseX, mouseY)){ // Check collision
+                            selectedTable = i;
+                            selectedTableX = t.getX();
+                            selectedTableY = t.getY();
+                            selected = true;
+                        }
+                    }
+                }
+            }
+            public void mouseReleased (MouseEvent e) {
+                
+                // Instatiate mouse coordinates 
+                int mouseX = e.getX() + MOUSE_X_OFFSET - offsetX;
+                int mouseY = e.getY() + MOUSE_Y_OFFSET - offsetY;
+
+                // Instatiate closest rectangle corner coordinates
+                int newTableX = ((mouseX - Table.WIDTH/2)/GRID_SQUARE_SIZE)*GRID_SQUARE_SIZE;
+                int newTableY = ((mouseY - Table.HEIGHT/2)/GRID_SQUARE_SIZE)*GRID_SQUARE_SIZE;
+                
+                // Check if table can be moved to new location
+                if (selected){
+                    boolean notOverlap = true;
+                    // Check all tables for collision
+                    for (int i = 0; i < numTables; i++){
+                        if (i != selectedTable){
+                            Table t = tableList.get(i);
+                            Rectangle tableA = new Rectangle(newTableX, newTableY, Table.WIDTH, Table.HEIGHT);
+                            Rectangle tableB = new Rectangle(t.getX(), t.getY(), Table.WIDTH, Table.HEIGHT);
+                            if (tableA.intersects(tableB)){
+                                notOverlap = false;
+                                break;
+                            }
+                        }
+                    }
+                    // If no collision, move to new location
+                    if (notOverlap){
+                        tableList.get(selectedTable).setX(newTableX);
+                        tableList.get(selectedTable).setY(newTableY);
+                    } else { // Otherwise, return to old position
+                        tableList.get(selectedTable).setX(selectedTableX);
+                        tableList.get(selectedTable).setY(selectedTableY);
+                    }
+                    selected = false;
+                    selectedTable = -1;
+                    selectedTableX = 0;
+                    selectedTableY = 0;
+                }
+            }
+            public void mouseClicked (MouseEvent e) {}
+            public void mouseEntered (MouseEvent e) {}
+            public void mouseExited (MouseEvent e) {}
+        }
+
+        // Mouse motion listener
+        private class MouseMotionEventListener implements MouseMotionListener {
+            public void mouseDragged (MouseEvent e) {
+                
+                // Instatiate mouse coordinates 
+                int mouseX = e.getX() + MOUSE_X_OFFSET - offsetX;
+                int mouseY = e.getY() + MOUSE_Y_OFFSET - offsetY;
+                
+                // If there is a table being moved, adjust its coordinates in relation to the mouse
+                if (selected){
+                    tableList.get(selectedTable).setX(mouseX - Table.WIDTH/2);
+                    tableList.get(selectedTable).setY(mouseY - Table.HEIGHT/2);
+                }
+            }
+            public void mouseMoved (MouseEvent e) {}
+        }
     }
 }
--- Table.java
+++ Table.java
@@ -1,12 +1,42 @@
+/******* Graphics imports *******/
 import java.awt.Graphics;
+import java.awt.Color;
 
+/**
+ * This class represents a table that contains a set amount of Student
+ * objects. This table can be drawn onto a java.awt.Graphics object.
+ * 
+ * @author Edison Du
+ * @author Peter Gu
+ * @author Jeffrey Xu
+ */
 public class Table {
 
+    /**
+     * The width of the table when drawn onto a Graphics object
+     */
+    public static final int WIDTH = 150;
+    /**
+     * The height of the table when drawn onto a Graphics object
+     */
+    public static final int HEIGHT = 90;
+    
+    /* Variables */
     private int id;
     private int capacity;
     private int numStudents;
+    private String group;
     private Student[] students;
 
+    /* Graphics related */
+    private int x, y;
+    private Color color;
+    
+    /**
+     * Constructs a new table that contains a set amount of Student objects
+     * @param id the id of the table, used to differenciate between tables
+     * @param capacity the maximum amount of Student objects that the table can contain
+     */
     Table(int id, int capacity) {
         this.id = id;
         this.capacity = capacity;
@@ -14,6 +44,11 @@ public class Table {
         this.students = new Student[capacity];
     }
 
+    /**
+     * This method is used to add a student into the table's array of students
+     * @param student the student to be added
+     * @return whether or not the student has been succesfully added
+     */
     public boolean addStudent(Student student) {
         if (this.seatsRemaining() == 0) {
             return false;
@@ -22,53 +57,230 @@ public class Table {
         return true;
     }
 
+    /**
+     * This method is used to remove a student from the table's array of students
+     * @param id the id of the student of be removed
+     * @return whether or not the student has been succesfully removed
+     */
     public boolean removeStudent(int id) {
         for (int i = 0; i < this.numStudents; i++) {
-            if (students[i].getId() == id) {
+            if (students[i].getID() == id) {
                 students[i] = null;
             }
-            // Shift students downwards in the array
-            if (i > 0 && students[i - 1] == null) {
-                students[i - 1] = students[i];
+            // Shift students downwards in the array once a student has been removed
+            if (i > 0 && students[i-1] == null) {
+                students[i-1] = students[i];
                 students[i] = null;
             }
         }
-        if (numStudents > 0 && students[numStudents - 1] == null) {
+        // Return true of the student exists and has been removed
+        if (numStudents > 0 && students[numStudents-1] == null) {
             numStudents--;
             return true;
         }
         return false;
     }
 
-    public boolean isEmpty() {
-        return this.numStudents == 0;
-    }
-
-    public int seatsRemaining() {
-        return this.capacity - this.numStudents;
-    }
-
-    public int getNumStudents() {
-        return this.numStudents;
-    }
-
-    public int getCapacity() {
-        return this.capacity;
-    }
-
-    public int getID() {
-        return this.id;
-    }
-
+    /**
+     * Returns an array containing the students in the table
+     * @return the array containing the students in the table
+     */
     public Student[] getStudents() {
         return this.students;
     }
 
-    public void draw(Graphics g, int x, int y) {
-        g.drawRect(x + 50, y + 50, 100, 100);
-        for (int i = 0; i < this.getNumStudents(); i++) {
-            g.drawString(Integer.toString(i + 1), x + 160, y * 200 + 70 + i * 20);
-            g.drawString(students[i].getName(), x + 170, y * 200 + 70 + i * 20);
+    /**
+     * Checks whether or not the table contains any students
+     * @return whether or not the table contains any students
+     */
+    public boolean isEmpty() {
+        return this.numStudents == 0;
+    }
+    
+    /**
+     * Returns the number of seats remaining in the table
+     * @return the number of seats remain
+     */
+    public int seatsRemaining() {
+        return this.capacity - this.numStudents;
+    }
+
+    
+    /**
+     * Returns the number of students at the table
+     * @return the number of students at the table
+     */
+    public int getNumStudents() {
+        return this.numStudents;
+    }
+
+    /**
+     * Returns the limit of students that the table can contain
+     * @return the limit of students that the table can contain
+     */
+    public int getCapacity() {
+        return this.capacity;
+    }
+    
+    /**
+     * Returns the id of the table
+     * @return the id of the table
+     */
+    public int getID() {
+        return this.id;
+    }
+
+    /**
+     * Modifies the id of the table
+     * @param id the id of the table to change to
+     */
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the group of the table
+     * @return the group of the table
+     */
+    public String getGroup() {
+        return this.group;
+    }
+
+    /**
+     * Changes the group associated with the table
+     * @param group the name of the group to change to
+     */
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    /**
+     * Returns the x position of the table
+     * @return the x position of the table
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Changes the x position of the table
+     * @param x the x position to change to
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Returns the y position of the table
+     * @return the y position of the table
+     */
+    public int getY() {
+        return this.y;
+    }
+    /**
+     * Changes the y position of the table
+     * @param y theS y position to change to
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * Returns a color object representing color of the table
+     * @return the color object representing color of the table
+     */
+    public Color getColor() {
+        return this.color;
+    }
+
+    /**
+     * Changes the color of the table
+     * @param color the color object to change to
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    /**
+     * This method checks if a point overlaps with a table
+     * @param x the x value of the point
+     * @param y the y value of the point
+     * @return whether or not the point overlaps with the table
+     */
+    public boolean checkOverlap(int x, int y) {
+        return ( (this.x <= x) && (x <= this.x + WIDTH) && (this.y <= y) && (y <= this.y + HEIGHT) );
+    }
+
+    /***** Graphics *****/
+
+    /**
+     * Display/draw the table onto a Graphics object
+     * @param g the graphics object for drawing
+     * @param xShift the horizontal offset of the tables when drawn
+     * @param yShift the vertical offset of the tables when drawn
+     */
+    public void draw(Graphics g, int xShift, int yShift) {
+        int nameGap = 23;
+        g.setColor(this.color);
+        g.fillRect(this.x + xShift, this.y + yShift, WIDTH, HEIGHT);
+        g.setColor(color.black);
+        g.drawRect(this.x + xShift, this.y + yShift, WIDTH, HEIGHT);
+        for (int i = 0; i < this.numStudents; i++){
+            g.setColor(new Color(30, 30, 30));
+            g.drawString(students[i].getName(), this.x + xShift + 5, this.y + yShift + i * nameGap + 15);
+        }
+    }
+
+    /**
+     * Display/draw the table onto a Graphics object with transparency
+     * @param g the graphics object for drawing
+     * @param xShift the horizontal offset of the tables when drawn
+     * @param yShift the vertical offset of the tables when drawn
+     */
+    public void drawTransparent(Graphics g, int xShift, int yShift) {
+        
+        int nameGap = 23;
+        
+        int red = this.color.getRed();
+        int green = this.color.getGreen();
+        int blue = this.color.getBlue();
+        int opacity = 125;
+        
+        g.setColor(new Color(red, green, blue, opacity));
+        g.fillRect(this.x + xShift, this.y + yShift, WIDTH, HEIGHT);
+        g.setColor(new Color(0, 0, 0, 125));
+        g.drawRect(this.x + xShift, this.y + yShift, WIDTH, HEIGHT);
+        for (int i = 0; i < this.numStudents; i++){
+            g.setColor(new Color(30, 30, 30, 125));
+            g.drawString(students[i].getName(), this.x + xShift + 5, this.y + yShift + i * nameGap + 15);
+        }
+    }
+
+    /**
+     * Display the info of the table clicked/selected by the user
+     * @param g the graphics object for drawing
+     * @param xShift the horizontal offset of the tables when drawn
+     * @param yShift the vertical offset of the tables when drawn
+     */
+    public void drawInformation(Graphics g, int xShift, int yShift) {
+        int nameGap = 23;
+        int informationWidth = 400;
+        int informationHeight = 150;
+
+        g.setColor(new Color(255, 255, 255, 130));
+        g.fillRect(xShift, yShift, informationWidth, informationHeight);
+        g.setColor(color.black);
+        g.drawRect(xShift, yShift, informationWidth, informationHeight);
+
+        String header = String.format("TABLE: %d | GROUP: %s", this.id, this.group);
+
+        g.drawString(header, xShift + 15, yShift  + nameGap);
+
+        for (int i = 0; i < this.numStudents; i++) {
+
+            String studentInformation = String.format("%d. ID: %d   |   NAME: %s", i+1, students[i].getID(), students[i].getName());
+
+            g.drawString(studentInformation, xShift + 15, yShift + 23 * (i+2));
         }
     }
 }
